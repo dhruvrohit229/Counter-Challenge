f00ee945d814b9f34ed09c02c09c2264b3919c7a
dhruvrohit229 <rohitdhruv205@gmail.com>
2025-08-13 08:10:22 +0000

    completed

--- packages/stylus-demo/src/lib.rs
+++ packages/stylus-demo/src/lib.rs
@@ -46,7 +46,7 @@ impl Counter {
     }
 
     /// Sets a number in storage to a user-specified value.
-    pub fn set_number(&mut self, new_number: U256) {
+    pub fn set_number_dhruv_rohit_229(&mut self, new_number: U256) {
         self.number.set(new_number);
     }
 
